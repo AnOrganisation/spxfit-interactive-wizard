e41df645eae6fa2e21fdd2500e3ae9bc977b9834
Divyansh Khare <dkhare@ucsc.edu>
2024-11-29 04:28:05 -0600

    Removed "image" from alt for accessibility benchmarks

--- components/overlayImages.tsx
+++ components/overlayImages.tsx
@@ -15,13 +15,13 @@ export default function OverlayImages({
   return (
     <>
       <img
-        alt="Metal Image"
+        alt="Metal"
         className="object-contain w-full h-full pointer-events-none select-none"
         src={activeSteelImage}
         draggable={false}
       />
       <img
-        alt="Upholstery Image"
+        alt="Upholstery"
         className="absolute top-0 left-0 object-contain w-full h-full pointer-events-none select-none"
         src={activeBenchImage}
         draggable={false}
